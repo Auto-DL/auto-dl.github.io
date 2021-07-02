b3fb9d971780ed3e98a0cbed8dfb6344b08bc8ac
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-07-03 02:49:00 +0530

    deleted react files

--- adl-website-latest/src/animations/animation.js
+++
@@ -1,8 +0,0 @@
-let headBg = "https://assets7.lottiefiles.com/packages/lf20_nhisz0kw.json";
-
-let tickAnima = {
-    blue: 'https://assets2.lottiefiles.com/packages/lf20_oxcwmxvz.json',
-    white: 'https://assets4.lottiefiles.com/packages/lf20_hsik5fyq.json'
-};
-
-export default {headBg, tickAnima};
