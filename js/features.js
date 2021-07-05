963bff43e5ced916468383077915ae5ec355510c
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-07-05 22:23:24 +0530

    sample github section added

--- js/features.js
+++ js/features.js
@@ -1,32 +1,30 @@
 document.querySelectorAll('.tick-animation').forEach(item => {
 
-    console.log("item", item)
-    item.addEventListener('load', () => {
+	// console.log("item", item)
+	// item.addEventListener('load', () => {
 
-        console.log("scrolled")
+	console.log("scrolled")
 
-        $(function() {
-            LottieInteractivity.create({
-                mode: 'scroll',
-                player: item,
-                // actions: [
-                // {
-                //     visibility: [0,1],
-                //     type: 'seek',
-                //     frames: [0, 121],
-                // }, ],
-                actions: [{
-                        visibility: [0, 0.2],
-                        type: "stop",
-                        frames: [0]
-                    },
-                    {
-                        visibility: [0.2, 1],
-                        type: "seek",
-                        frames: [0, 121]
-                    }
-                ],
-            })
-        })
-    })
+	LottieInteractivity.create({
+		mode: 'scroll',
+		player: item.id,
+		// actions: [
+		// {
+		//     visibility: [0,1],
+		//     type: 'seek',
+		//     frames: [0, 121],
+		// }, ],
+		actions: [{
+				visibility: [0, 0.2],
+				type: "stop",
+				frames: [0]
+			},
+			{
+				visibility: [0.2, 1],
+				type: "seek",
+				frames: [0, 121]
+			}
+		],
+	})
+	//     })
 })
