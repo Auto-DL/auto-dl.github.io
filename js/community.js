713e2f4d8c1fa8688957245bb948850a3217555b
Vedant Sarnobat <vedant.sarnobat@somaiya.edu>
2021-09-17 20:56:27 +0530

    community page suggestions

--- js/community.js
+++ js/community.js
@@ -1,20 +1,50 @@
 $(window).on('load', async () => {
 
-	// await $.get("https://vedant080102.github.io/auto-dl.github.io/content/community.yml", (text, status) => {
-	await $.get("../content/community.yml", (text, status) => {
+	await $.get("https://vedant080102.github.io/auto-dl.github.io/content/community.yml", (text, status) => {
+	// await $.get("../content/community.yml"~, (text, status) => {
 		console.log("Data Status: " + status);
-		
+
 		// Get document, or throw exception on error
 		try {
 			const data = jsyaml.load(text);
 			console.log(data);
-			// Footer(data.footerData);
+			displayComData(data.content);
 		} catch (e) {
 			console.log(e);
 		}
 	});
 })
 
-// function displayComData(doc) {
-//     var data = ``
-// }
+function displayComData(doc) {
+	var data = ``
+
+	data = `
+	<div class="container-fluid container-xl">
+		<div class="row row-cols-1 row-cols-md-3">
+	`
+
+	doc.forEach(item => {
+		data += `
+		<div class="card-container col">
+			<div class="card ${item.theme}-bg">
+				<!-- <div class="card-header"></div> -->
+				<!-- <a style='text-decoration: none; color: inherit' href=${item.link}> -->
+				<div class="card-body">
+					<img class="card-img" src=${item.icon} height="25" alt=${item.title}>
+					<div class="card-title">${item.title}</div>
+					<!-- <div class="card-text">Meet other Auto-DL users and share your experiences by attending events and meetups.</div> -->
+					<a class="btn ${item.theme === 'light' ? 'dark': 'light'}-btn rounded-pill" 
+						target="_blank" href=${item.link}>Learn More</a>
+				</div>
+				<!-- </a> -->
+			</div>
+		</div>
+		`
+	});
+
+	data += `
+		</div>
+	</div>`
+
+	document.getElementById("com-cards").innerHTML = data;
+}
